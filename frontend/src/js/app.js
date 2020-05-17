112c2d522f596d7a07906d41b4e28f27ac4564f3
Artem <agbusko@gmail.com>
2020-05-17 21:51:17 +0300

    AB: домашка Лаврика №1

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -7,30 +7,68 @@ new Vue({
     data: {
         info: [
             {
-                name: 'Artem',
+                name: 'Name',
                 value: '',
-                pattern: /^[a-zA-Z ]{2,30}$/
+                pattern: /^[a-zA-Z]+$/
             },
             {
                 name: 'Phone',
                 value: '',
-                pattern: /^[0-9]{7,14}$/
+                pattern: /^[0-9]+$/
             },
             {
                 name: 'Email',
                 value: '',
-                pattern: /.+/
+                pattern: /^[a-zA-Z]+$/
             },
             {
                 name: 'Some Field 1',
                 value: '',
-                pattern: /.+/
+                pattern: /^[a-zA-Z]+$/
             },
             {
                 name: 'Some Field 2',
                 value: '',
-                pattern: /.+/
+                pattern: /^[a-zA-Z]+$/
             }
-        ]
+        ],
+        controls: [],
+        formSubmited: false
+    },
+    created(){
+        for (let i = 0; i < this.info.length; i++){
+            this.controls.push({
+                error: !this.info[i].pattern.test(this.info[i].value),
+                activated: this.info[i].value != ''
+            });
+        }
+    },
+    methods: {
+        onInput(index, value){
+            let data = this.info[index];
+            let control = this.controls[index];
+
+            data.value = value;
+            control.error = !data.pattern.test(value);
+            control.activated = true;
+        }
+    },
+    computed: {
+        done(){
+            let done = 0;
+
+            for (let i = 0; i < this.controls.length; i++){
+                if (!this.controls[i].error){
+                    done++;
+                }
+            }
+
+            return done;
+        },
+        progressWidth(){
+            return {
+                width: (this.done / this.info.length * 100) + '%'
+            };
+        }
     }
 });
